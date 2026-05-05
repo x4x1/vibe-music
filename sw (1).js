194c35f44d82044332579b8053568a68297aeafb
x4x1 <ahintl.ksa@gmail.com>
2026-05-06 01:24:50 +0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'vibe-music-v1';
+const urlsToCache = [
+  '/vibe-music/',
+  '/vibe-music/index.html',
+  '/vibe-music/manifest.json',
+  '/vibe-music/sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).catch(() => caches.match('/vibe-music/'));
+      })
+  );
+});
